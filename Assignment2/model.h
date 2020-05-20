ec8baaa9088dd7dd284b567d09104cee3d855860
Avani Chandorkar <avanichandorkar22@gmail.com>
2020-05-20 14:02:37 -0500

    Add files via upload

---
+++ Assignment2/model.h
@@ -0,0 +1,28 @@
+// Chandorkar, Avani
+// arc8554
+// 2020-04-21
+
+#if !defined( __MODEL_H__ )
+#define __MODEL_H__
+
+//----------------------------------------------------------
+#include "vertex.h"
+#include "face.h"
+
+//----------------------------------------------------------
+typedef struct _Model {
+  int     m_numVertices;
+  int     m_numFaces;
+  Vertex *m_vertex;
+  Face   *m_face;
+} Model;
+
+//----------------------------------------------------------
+Model *allocModel( int numVertices, int numFaces );
+void   dumpModel( Model *model );
+void   freeModel( Model *m );
+Model *loadModel( char *fName );
+
+//----------------------------------------------------------
+#endif
+
---
+++ Assignment2/vertex.h
@@ -0,0 +1,20 @@
+// Chandorkar, Avani
+// arc8554
+// 2020-04-21
+
+#if !defined( __VERTEX_H__ )
+#define __VERTEX_H__
+
+//----------------------------------------------------------
+typedef struct _Vertex {
+  double x;
+  double y;
+  double z;
+} Vertex;
+
+//----------------------------------------------------------
+void   dumpVertex( Vertex *vertex );
+
+//----------------------------------------------------------
+#endif
+
